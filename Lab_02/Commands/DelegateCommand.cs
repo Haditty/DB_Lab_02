389b5eb561da2b390232f58964003c4868822574
HADI-ITHÖGSKOLA\hadid <hadi.daliri@iths.se>
2026-01-02 21:36:21 +0100

    DelegateCommand class added

---
+++ Lab_02/Commands/DelegateCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab_02.Commands
+{
+    internal class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public DelegateCommand(Action<object> execute, Func<object?, bool>? canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute is null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this,EventArgs.Empty);
+    }
+}
